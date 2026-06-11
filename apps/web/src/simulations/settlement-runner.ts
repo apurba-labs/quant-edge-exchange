322fdc0089c06fa2d4417d2b9afd2fd7c8a88532
Apurba Narayan Singh <apurbansinghdev@gmail.com>
2026-06-12 00:25:42 +0600

    Revert "feat: add observability dashboard metrics"

--- apps/web/src/simulations/settlement-runner.ts
+++
@@ -1,9 +0,0 @@
-import { Bid } from "./bid-generator";
-
-export async function settleBid( bid: Bid ) {
-  return {
-    settled: true,
-    winningBid: bid.bidAmount,
-    settlementTime: new Date(),
-  };
-}
--- apps/web/src/simulations/test.ts
+++
@@ -1,9 +0,0 @@
-import { runSimulation } from "./simulator";
-
-async function main() {
-  const result = await runSimulation();
-
-  console.log(result);
-}
-
-main();
